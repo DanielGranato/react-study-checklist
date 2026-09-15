e7f8f3d0f2252425bb82cfcd1c9f5d8eebedd5a6
Daniel Granato <dgmgranato@gmail.com>
2026-09-15 14:23:24 -0400

    add form, text input

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useRef } from "react";
 
 import './dialog.style.css'
+import { IconClose } from "../icons";
 
 export function Dialog({ isOpen, onClose, children }) {
+    // não deveríamos fazer buscas no DOM desse jeito!
+    // const dialog = document.querySelector("dialog");
+
     const dialogRef = useRef(null)
 
     useEffect(() => {
@@ -13,20 +17,32 @@ export function Dialog({ isOpen, onClose, children }) {
         }
     }, [isOpen])
 
+    // "Show the dialog" button opens the dialog modally
     const openDialog = () => {
         dialogRef.current.showModal();
     };
 
+    // "Close" button closes the dialog
     const closeDialog = () => {
         dialogRef.current.close();
     };
 
     return (
-        <>
-            <dialog ref={dialogRef}>
-                <button autoFocus onClick={onClose}>Close</button>
-                <p>This modal dialog has a groovy backdrop!</p>
-            </dialog>        
-        </>
+        <React.Fragment>
+            <dialog ref={dialogRef} className="dialog">
+                <div className="btn-close-wrapper">
+                    <button 
+                        autoFocus 
+                        onClick={onClose} 
+                        className="btn-close"
+                    >
+                        <IconClose />
+                    </button>
+                </div>
+                <div className="body">
+                    {children}
+                </div>
+            </dialog>
+        </React.Fragment>
     )
 }
